a3aa5dd9910771878da3da34290c41cb0f3641f4
Yuliia Ataman <47396774+chandrashka@users.noreply.github.com>
2024-01-02 16:20:13 +0100

    Add files via upload

---
+++ export_csv.py
@@ -0,0 +1,33 @@
+import csv
+import psycopg2
+
+# Підключення до бази даних PostgreSQL
+conn = psycopg2.connect(
+    dbname='spotify', 
+    user='Ataman',  
+    password='postgres',
+    host='localhost',
+)
+
+
+cursor = conn.cursor()
+
+# Запити для отримання даних з таблиць
+tables = ['Track', 'Artist', 'Playlist', 'Charts', 'Streams']
+
+with open('exported_data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+
+    for table in tables:
+        query = f"SELECT * FROM {table};"
+        cursor.execute(query)
+
+        # Перевірка, чи є результати запиту
+        if cursor.description:
+            writer.writerow([desc[0] for desc in cursor.description])
+            writer.writerows(cursor.fetchall())
+        else:
+            print(f"No data returned for the {table} table")
+
+# Закриття з'єднання з базою даних
+conn.close()
